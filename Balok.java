62c0fee74469c397979174b92ff9756da9cc63b0
ari bhawanaputra <117577163+ariputraa666@users.noreply.github.com>
2024-06-01 21:50:45 +0800

    Add files via upload

---
+++ Balok.java
@@ -0,0 +1,58 @@
+package projectvolumebalok;
+/**
+ *
+ * @author ariputraaa
+ */
+public class Balok {
+  // Atribut (Private)
+  private double pj;
+  private double lb;
+  private double tg;
+
+  // Constructor
+  public Balok(double pj, double lb, double tg) {
+    this.pj = pj;
+    this.lb = lb;
+    this.tg = tg;
+  }
+
+  // Setter untuk Panjang (Public)
+  public void setPj(double npj) {
+    pj = npj;
+  }
+
+  // Setter untuk Lebar (Public)
+  public void setLb(double nlb) {
+    lb = nlb;
+  }
+
+  // Setter untuk Tinggi (Public)
+  public void setTg(double ntg) {
+    tg = ntg;
+  }
+
+  // Hitung Volume (Public)
+  public double hitungVolume() {
+    return pj * lb * tg;
+  }
+
+  // Cetak Hasil (Public)
+  public void printHasil() {
+    System.out.println("Panjang: " + pj);
+    System.out.println("Lebar: " + lb);
+    System.out.println("Tinggi: " + tg);
+    System.out.println("Volume: " + hitungVolume());
+  }
+
+  // Metode Tambahan 1: Hitung Luas Permukaan
+  public double hitungLuasPermukaan() {
+    return 2 * (pj * lb + pj * tg + lb * tg);
+  }
+
+  // Metode Tambahan 2: Hitung Luas Alas
+  public double hitungLuasAlas() {
+    return pj * lb;
+  }
+}
+
+
---
+++ Main.java
@@ -0,0 +1,35 @@
+package projectvolumebalok;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ariputraaa
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    // Inputan Pengguna
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Masukkan Panjang: ");
+    double pj = scanner.nextDouble();
+    System.out.print("Masukkan Lebar: ");
+    double lb = scanner.nextDouble();
+    System.out.print("Masukkan Tinggi: ");
+    double tg = scanner.nextDouble();
+
+    // Buat Objek Balok
+    Balok balok = new Balok(pj, lb, tg);
+
+    // Cetak Hasil
+    balok.printHasil();
+
+    // Hitung dan Cetak Luas Permukaan
+    double luasPermukaan = balok.hitungLuasPermukaan();
+    System.out.println("Luas Permukaan: " + luasPermukaan);
+
+    // Hitung dan Cetak Luas Alas
+    double luasAlas = balok.hitungLuasAlas();
+    System.out.println("Luas Alas: " + luasAlas);
+  }
+}
+
